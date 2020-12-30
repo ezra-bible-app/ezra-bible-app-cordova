d7a3c38d1d028d73855a4b0cb64daabdb315d9ab
Tobias Klein <contact@tklein.info>
2020-12-30 09:55:18 +0100

    main.js: Move pause and resume event handlers to init function

--- www/nodejs-project/main.js
+++ www/nodejs-project/main.js
@@ -10,52 +10,19 @@ function init() {
 
   console.log("Initializing IPC!");
   ipc.init();
+
+  // Handle the 'pause' and 'resume' events.
+  // These are events raised automatically when the app switched to the
+  // background/foreground.
+  cordova.app.on('pause', (pauseLock) => {
+    console.log('[node] app paused.');
+    pauseLock.release();
+  });
+
+  cordova.app.on('resume', () => {
+    console.log('[node] app resumed.');
+    cordova.channel.post('engine', 'resumed');
+  });
 }
 
 init();
-
-// Handle the 'pause' and 'resume' events.
-// These are events raised automatically when the app switched to the
-// background/foreground.
-cordova.app.on('pause', (pauseLock) => {
-  console.log('[node] app paused.');
-  pauseLock.release();
-});
-
-cordova.app.on('resume', () => {
-  console.log('[node] app resumed.');
-  cordova.channel.post('engine', 'resumed');
-});
-
-/*
-// Post an event to Cordova.
-cordova.channel.post('started');
-
-// Post an event with a message.
-cordova.channel.post('started', 'main.js loaded');
-
-// Listen to messages from Cordova.
-cordova.channel.on('message', (msg) => {
-  console.log('[node] MESSAGE received: "%s"', msg);
-  // Reply sending a user defined object.
-  cordova.channel.send(new Reply('Message received!', msg));
-});
-
-// Listen to event 'myevent' from Cordova.
-cordova.channel.on('myevent', (msg) => {
-  console.log('[node] MYEVENT received with message: "%s"', msg);
-});
-
-// Handle the 'pause' and 'resume' events.
-// These are events raised automatically when the app switched to the
-// background/foreground.
-cordova.app.on('pause', (pauseLock) => {
-  console.log('[node] app paused.');
-  pauseLock.release();
-});
-
-cordova.app.on('resume', () => {
-  console.log('[node] app resumed.');
-  cordova.channel.post('engine', 'resumed');
-});
-*/
