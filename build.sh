6ff925b53c5286393d4fb5ca5a905964bdf97bf9
Tobias Klein <contact@tklein.info>
2021-08-29 09:37:54 +0200

    Run bundle instead of browserify (no minification needed)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 npm --prefix ezra-bible-app uninstall node-sword-interface
 npm --prefix ezra-bible-app run compile-pug
-npm --prefix ezra-bible-app run browserify
+npm --prefix ezra-bible-app run bundle
 npm --prefix www/nodejs-project install --ignore-scripts
 
 git clone https://github.com/karlkleinpaste/biblesync.git www/nodejs-project/node_modules/node-sword-interface/biblesync
