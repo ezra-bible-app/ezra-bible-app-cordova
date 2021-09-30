2ad290a5ee41cd644f78da18c5e983a6725cf2fa
Tobias Klein <contact@tklein.info>
2021-09-30 15:04:06 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $SCRIPT_DIR
+SCRIPT_DIR=$(dirname "$0")
 
-npm --prefix ezra-bible-app uninstall --unsafe-perm=true --allow-root node-sword-interface
-npm --prefix ezra-bible-app run compile-pug
-npm --prefix ezra-bible-app run bundle
-npm --prefix www/nodejs-project install --ignore-scripts
+npm --prefix $SCRIPT_DIR/ezra-bible-app uninstall --unsafe-perm=true --allow-root node-sword-interface
+npm --prefix $SCRIPT_DIR/ezra-bible-app run compile-pug
+npm --prefix $SCRIPT_DIR/ezra-bible-app run bundle
+npm --prefix $SCRIPT_DIR/www/nodejs-project install --ignore-scripts
 
 BIBLE_SYNC_DIR=$SCRIPT_DIR/www/nodejs-project/node_modules/node-sword-interface/biblesync
 
