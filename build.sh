372889a1ec93bbd2179f8541cf674fbc661a5284
Tobias Klein <contact@tklein.info>
2021-09-30 14:56:53 +0200

    Optimize build script for running in Docker

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-npm --prefix ezra-bible-app uninstall node-sword-interface
+SCRIPT_DIR=$(dirname "$0")
+cd $SCRIPT_DIR
+
+npm --prefix ezra-bible-app uninstall --unsafe-perm=true --allow-root node-sword-interface
 npm --prefix ezra-bible-app run compile-pug
-npm --prefix ezra-bible-app run browserify
+npm --prefix ezra-bible-app run bundle
 npm --prefix www/nodejs-project install --ignore-scripts
 
-git clone https://github.com/karlkleinpaste/biblesync.git www/nodejs-project/node_modules/node-sword-interface/biblesync
-git -C www/nodejs-project/node_modules/node-sword-interface/biblesync checkout 2.1.0
+BIBLE_SYNC_DIR=$SCRIPT_DIR/www/nodejs-project/node_modules/node-sword-interface/biblesync
+
+rm -r $BIBLE_SYNC_DIR
+git clone https://github.com/karlkleinpaste/biblesync.git $BIBLE_SYNC_DIR
+git -C $BIBLE_SYNC_DIR checkout 2.1.0
 
 cordova prepare
 cordova build
